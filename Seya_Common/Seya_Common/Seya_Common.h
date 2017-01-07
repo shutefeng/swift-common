54993e41bafa35af7992dc20cc3b50ddb4bb95c4
TT <196159353@qq.com>
2017-01-07 22:59:25 +0800

    Cc 提交lib 工程

---
+++ Seya_Common/Seya_Common/Seya_Common.h
@@ -0,0 +1,19 @@
+//
+//  Seya_Common.h
+//  Seya_Common
+//
+//  Created by TT on 2017/1/7.
+//  Copyright © 2017年 TT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Seya_Common.
+FOUNDATION_EXPORT double Seya_CommonVersionNumber;
+
+//! Project version string for Seya_Common.
+FOUNDATION_EXPORT const unsigned char Seya_CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Seya_Common/PublicHeader.h>
+
+
